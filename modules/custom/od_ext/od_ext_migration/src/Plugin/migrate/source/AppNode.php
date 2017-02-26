b164b1f4b42c7f05ee0e8961324e8adcfcfbe297
William Hearn <sylus1984@gmail.com>
2017-02-26 18:50:16 -0500

    feat(migrate): Logic for handling content entities from external db

---
+++ modules/custom/od_ext/od_ext_migration/src/Plugin/migrate/source/AppNode.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace Drupal\od_ext_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for app content.
+ *
+ * @MigrateSource(
+ *   id = "app_node"
+ * )
+ */
+class AppNode extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('node', 'n')
+                  ->fields('n', ['nid', 'vid', 'language', 'title'])
+                  ->condition('n.type', 'apps');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'nid' => $this->t('Node ID'),
+      'vid' => $this->t('Revision ID'),
+      'language' => $this->t('Language'),
+      'title' => $this->t('Title'),
+      'body' => $this->t('Body'),
+      'name' => $this->t('Name'),
+      'url' => $this->t('URL'),
+      'dev' => $this->t('Developer Name'),
+      'submitter' => $this->t('Submitter'),
+      'date_pub' => $this->t('Date Published'),
+      'file_fid' => $this->t('File fid'),
+      'file_alt' => $this->t('File alt'),
+      'file_title' => $this->t('File title'),
+      'app_categories' => $this->t('Categories'),
+      'app_freetags' => $this->t('Tags'),
+      'community' => $this->t('Community'),
+      'departments' => $this->t('Departments'),
+      'device_formats' => $this->t('Device Formats'),
+      'ribbon' => $this->t('Ribbon'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'nid' => [
+        'type' => 'integer',
+        'alias' => 'n',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+
+    // Body.
+    $body = $this->select('field_data_body', 'db')
+                 ->fields('db', ['body_value'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchCol();
+
+    // Application Name.
+    $name = $this->select('field_data_field_application_name', 'df')
+                 ->fields('df', ['field_application_name_value'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchCol();
+
+    // Application URL.
+    $url = $this->select('field_data_field_application_url', 'df')
+                 ->fields('df', ['field_application_url_url'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchCol();
+
+    // Application Developer.
+    $dev = $this->select('field_data_field_application_developer', 'df')
+                 ->fields('df', ['field_application_developer_value'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchCol();
+
+    // App Submitter Name.
+    $submitter = $this->select('field_data_field_application_submitter_name', 'df')
+                 ->fields('df', ['field_application_submitter_name_value'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchCol();
+
+    // Date Published.
+    $date_pub = $this->select('field_data_field_date_published', 'df')
+                 ->fields('df', ['field_date_published_value'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', 'und')
+                 ->execute()
+                 ->fetchCol();
+
+    // Screenshots.
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $file = $this->select('field_data_field_application_screenshots', 'df')
+                 ->fields('df', ['field_application_screenshots_fid',
+                                 'field_application_screenshots_alt',
+                                 'field_application_screenshots_title'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->execute()
+                 ->fetchAssoc();
+
+    // App Categories (Subject).
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $app_categories = $this->select('field_data_field_application_categories', 'df')
+                 ->fields('df', ['field_application_categories_target_id'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', 'und')
+                 ->execute()
+                 ->fetchAssoc();
+
+    // App Tags (Keywords).
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $app_tags = $this->select('field_data_field_app_tags', 'df')
+                 ->fields('df', ['field_app_tags_tid'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchAssoc();
+
+    // Community.
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $community = $this->select('field_data_field_community', 'df')
+                 ->fields('df', ['field_community_tid'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', 'und')
+                 ->execute()
+                 ->fetchAssoc();
+
+    // Departments.
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $departments = $this->select('field_data_field_departments', 'df')
+                 ->fields('df', ['field_departments_target_id'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchAssoc();
+
+    // Device Formats.
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $device_formats = $this->select('field_data_field_device_formats', 'df')
+                 ->fields('df', ['field_device_formats_target_id'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', 'und')
+                 ->execute()
+                 ->fetchAssoc();
+
+    // Ribbon.
+    // TODO: switch to fetchAllAssoc + remap in YML.
+    $ribbon = $this->select('field_data_field_ribbon', 'df')
+                 ->fields('df', ['field_ribbon_tid'])
+                 ->condition('entity_id', $row->getSourceProperty('nid'))
+                 ->condition('revision_id', $row->getSourceProperty('vid'))
+                 ->condition('language', $row->getSourceProperty('language'))
+                 ->execute()
+                 ->fetchAssoc();
+
+    $row->setSourceProperty('body', $body[0]);
+    $row->setSourceProperty('name', $name[0]);
+    $row->setSourceProperty('url', $url[0]);
+    $row->setSourceProperty('dev', $dev[0]);
+    $row->setSourceProperty('submitter', $submitter[0]);
+    $row->setSourceProperty('date_pub', $date_pub[0]);
+    $row->setSourceProperty('app_categories', $app_categories['field_application_categories_target_id']);
+    $row->setSourceProperty('app_freetags', $app_tags['field_app_tags_tid']);
+    $row->setSourceProperty('community', $community['field_community_tid']);
+    $row->setSourceProperty('departments', $departments['field_departments_target_id']);
+    $row->setSourceProperty('device_formats', $device_formats['field_device_formats_target_id']);
+    $row->setSourceProperty('ribbon', $ribbon['field_ribbon_tid']);
+    $row->setSourceProperty('file_fid', $file['field_application_screenshots_fid']);
+    $row->setSourceProperty('file_alt', $file['field_application_screenshots_alt']);
+    $row->setSourceProperty('file_title', $file['field_application_screenshots_title']);
+
+    return parent::prepareRow($row);
+  }
+
+}
---
+++ modules/custom/od_ext/od_ext_migration/src/Plugin/migrate/source/FileManaged.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\od_ext_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for file content.
+ *
+ * @MigrateSource(
+ *   id = "file_managed"
+ * )
+ */
+class FileManaged extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('file_managed', 'f')
+                  ->fields('f', ['fid', 'filename', 'uri']);
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'fid' => $this->t('File ID'),
+      'filename' => $this->t('Filename'),
+      'uri' => $this->t('URI'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'fid' => [
+        'type' => 'integer',
+        'alias' => 'f',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+
+    return parent::prepareRow($row);
+  }
+
+}
