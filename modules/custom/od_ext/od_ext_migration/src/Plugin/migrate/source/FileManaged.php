e716cb9c2b0e42fa8fcf7104eb086b82fe3447d6
William Hearn <sylus1984@gmail.com>
2017-04-03 23:38:31 -0400

    feat(migrate): media entity support for migrated images

--- modules/custom/od_ext/od_ext_migration/src/Plugin/migrate/source/FileManaged.php
+++ modules/custom/od_ext/od_ext_migration/src/Plugin/migrate/source/FileManaged.php
@@ -18,13 +18,8 @@ class FileManaged extends SqlBase {
    */
   public function query() {
     $query = $this->select('file_managed', 'f')
-      ->fields('f',
-      [
-        'fid',
-        'filename',
-        'uri',
-      ]
-    );
+      ->fields('f')
+      ->orderBy('f.fid');
 
     return $query;
   }
@@ -35,8 +30,13 @@ class FileManaged extends SqlBase {
   public function fields() {
     $fields = [
       'fid' => $this->t('File ID'),
+      'uid' => $this->t('The {users}.uid who added the file. If set to 0, this file was added by an anonymous user.'),
       'filename' => $this->t('Filename'),
       'uri' => $this->t('URI'),
+      'filemime' => $this->t('File MIME Type'),
+      'status' => $this->t('The published status of a file.'),
+      'timestamp' => $this->t('The time that the file was added.'),
+      'type' => $this->t('The type of this file.'),
     ];
 
     return $fields;
